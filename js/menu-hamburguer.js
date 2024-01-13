71b54b200700a16c0bebd408acb1edb89c1c4ab2
Matheus Gustavo <matheusgustavosprog@gmail.com>
2024-01-13 19:28:28 -0300

    menu mobile

--- js/menu-hamburguer.js
+++ js/menu-hamburguer.js
@@ -1,11 +1,28 @@
 const btnMobile = document.getElementById("btn-mobile");
+const nav = document.getElementById("nav");
 
 function toggleMenu(event) {
-    if(event.type === 'touchstart') event.preventDefault()
-    const nav = document.getElementById("nav");
-    nav.classList.toggle('ativo')
-    event.curretTarget.setAttribute('aria-expanded', 'true')
+    if (event.type === 'touchstart') event.preventDefault();
+
+    nav.classList.toggle('ativo');
+    const ativo = nav.classList.contains("ativo");
+    btnMobile.setAttribute('aria-expanded', ativo);
+    btnMobile.setAttribute('aria-label', ativo ? "Fechar Menu" : "Abrir Menu");
+}
+
+function closeMenu() {
+    nav.classList.remove('ativo');
+    btnMobile.setAttribute('aria-expanded', false);
+    btnMobile.setAttribute('aria-label', "Abrir Menu");
+}
+
+function clickMobile(event) {
+    const tagName = event.target.tagName.toLowerCase();
+    if (tagName === 'a' || tagName === 'ul' || tagName === 'li') {
+        closeMenu();
+    }
 }
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+nav.addEventListener('click', clickMobile);
